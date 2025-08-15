773d8dfe6edfe74d0859aa712b9436133db2fd8c
Michael Stowe <me@mkstowe.com>
2025-08-15 14:26:57 -0400

    Environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    etsyUrl: "https://MaisyMilesDesigns.etsy.com",
+    instagramUrl: "https://instagram.com/MaisyMilesDesigns",
+    facebookUrl: "https://facebook.com/MaisyMilesDesigns",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    etsyUrl: "https://MaisyMilesDesigns.etsy.com",
+    instagramUrl: "https://instagram.com/MaisyMilesDesigns",
+    facebookUrl: "https://facebook.com/MaisyMilesDesigns",
+};
